a104c39b52241c3da6ef73af84b47f031998c006
Maciej <maciek1234599@gmail.com>
2020-12-06 02:29:56 +0100

    Add solution for part two of Day 2 puzzle

--- Day 2/Program.cs
+++ Day 2/Program.cs
@@ -7,10 +7,11 @@ namespace Day_2
         static void Main(string[] args)
         {
             string[] splittedInput = System.IO.File.ReadAllText("input.txt").Split('\n');
-            Console.WriteLine(ValidPasswords(splittedInput)); // solution for the first part
+            Console.WriteLine(ValidPasswordsObsolete(splittedInput)); // solution for the first part
+            Console.WriteLine(ValidPasswords(splittedInput)); // solution for the second part
         }
 
-        static Tuple<int, int, char, string> Separate(string passwordLine)
+        static Tuple<int, int, char, string> Separate(string passwordLine)  // separating info from a single line of input to a tuple
         {
             var buffer = String.Empty;
             int minNumber = 0, maxNumber = 0;
@@ -45,22 +46,38 @@ namespace Day_2
             return Tuple.Create(minNumber, maxNumber, requiredLetter, buffer);
         }
 
-        static int ValidPasswords(string[] input)
+        static int ValidPasswordsObsolete(string[] input) // counting passwords that have given number of ocurrences of a certain characters
         {
             var passwordCounter = 0;
-            
+
             foreach (var passwordLine in input)
             {
                 var separatedPasswordLine = Separate(passwordLine);
                 var letterCounter = 0;
 
-                foreach (var letter in separatedPasswordLine.Item4)
+                foreach (var letter in separatedPasswordLine.Item4) // loop counting number of ocurrences of a character
                 {
-                    if (letter == separatedPasswordLine.Item3)
+                    if (letter == separatedPasswordLine.Item3) 
                         letterCounter++;
                 }
 
-                if (letterCounter >= separatedPasswordLine.Item1 && letterCounter <= separatedPasswordLine.Item2)
+                if (letterCounter >= separatedPasswordLine.Item1 && letterCounter <= separatedPasswordLine.Item2) // checking if number of ocurrences fulfills the condition
+                    passwordCounter++;
+            }
+
+            return passwordCounter;
+        }
+
+        static int ValidPasswords(string[] input) // counting passwords that have a certain character in one (and only one) of given positions  
+        {
+            var passwordCounter = 0;
+
+            foreach (var passwordLine in input)
+            {
+                var separatedPasswordLine = Separate(passwordLine);
+
+                if ((separatedPasswordLine.Item4[separatedPasswordLine.Item1 - 1] == separatedPasswordLine.Item3)
+                    != (separatedPasswordLine.Item4[separatedPasswordLine.Item2 - 1] == separatedPasswordLine.Item3))
                     passwordCounter++;
             }
 
