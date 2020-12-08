72fb094a29cf11496b949ff4c17dc1e1bad7f0d4
Maciej <maciek1234599@gmail.com>
2020-12-08 13:56:21 +0100

    Add solution for part one of day 6 puzzle

---
+++ Day 6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var splittedInput = System.IO.File.ReadAllText("input.txt").Split("\n\n");
+            Console.WriteLine(YesAnswers(splittedInput)); // solution to part one
+        }
+
+        static int YesAnswers(string[] input)
+        {
+            var answeredQuestions = new List<char>();
+            var answeredCount = 0;
+
+            foreach (var group in input)
+            {
+                foreach (var question in group)
+                {
+                    if (!answeredQuestions.Contains(question) && Char.IsLower(question))
+                        answeredQuestions.Add(question); // it contains only unique occurances of questions
+                }
+                answeredCount += answeredQuestions.Count;
+                answeredQuestions.Clear(); // clear before checking next group
+            }
+            return answeredCount;
+        }
+    }
+}
