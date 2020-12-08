8406d49df6fba2468896308fcaf037e8dc3db119
Maciej <maciek1234599@gmail.com>
2020-12-08 01:03:07 +0100

    Add solution for part one of day 4 puzzle

---
+++ Day 4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace Day_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var splittedInput = System.IO.File.ReadAllText("input.txt").Split("\n\n");
+            Console.WriteLine(ValidPassports(splittedInput)); // solution for part one
+        }
+
+        static int ValidPassports(string[] input)
+        {
+            var numberOfValid = 0;
+
+            foreach (var passport in input)
+            {
+                var splittedPassport = passport.Split(' ', '\n'); // split all entries of each passport
+                if ((!splittedPassport.Any(s => s.Contains("cid")) && splittedPassport.Length == 7) || splittedPassport.Length == 8)
+                    numberOfValid++; // passport is valid if it contains all entries or is missing only a cid entry
+            }
+
+            return numberOfValid;
+        }
+    }
+}
